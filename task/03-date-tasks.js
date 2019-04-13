6a4da024008a616625d527f0ab429d571f773b34
antoprokopovich <antoprokopovich@gmail.com>
2019-04-14 00:50:31 +0300

    4 tasks solved

--- task/03-date-tasks.js
+++ task/03-date-tasks.js
@@ -22,7 +22,7 @@
  *    'Sun, 17 May 1998 03:00:00 GMT+01' => Date()
  */
 function parseDataFromRfc2822(value) {
-   throw new Error('Not implemented');
+   return Date.parse(value);
 }
 
 /**
@@ -37,7 +37,7 @@ function parseDataFromRfc2822(value) {
  *    '2016-01-19T08:07:37Z' => Date()
  */
 function parseDataFromIso8601(value) {
-   throw new Error('Not implemented');
+   return Date.parse(value);
 }
 
 
@@ -56,7 +56,13 @@ function parseDataFromIso8601(value) {
  *    Date(2015,1,1)    => false
  */
 function isLeapYear(date) {
-   throw new Error('Not implemented');
+   let year = date.getFullYear();
+
+   if ((year % 400 == 0 || year % 100 != 0) && year % 4 == 0) {
+      return true;
+   } else {
+      return false;
+   }
 }
 
 
@@ -76,7 +82,16 @@ function isLeapYear(date) {
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,15,20,10,453)   => "05:20:10.453"
  */
 function timeSpanToString(startDate, endDate) {
-   throw new Error('Not implemented');
+   let hours = endDate.getHours() - startDate.getHours();
+   let min = endDate.getMinutes() - startDate.getMinutes();
+   let sec = endDate.getSeconds() - startDate.getSeconds();
+   let ms = endDate.getMilliseconds() - startDate.getMilliseconds();
+   let date = new Date(2000,1,1,0,0,0,0);
+   date.setHours(hours + 3);
+   date.setMinutes(min);
+   date.setSeconds(sec);
+   date.setMilliseconds(ms);
+   return date.toISOString().slice(11, -1);
 }
 
 
@@ -94,7 +109,9 @@ function timeSpanToString(startDate, endDate) {
  *    Date.UTC(2016,3,5,21, 0) => Math.PI/2
  */
 function angleBetweenClockHands(date) {
-    throw new Error('Not implemented');
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    return 0.5 * Math.PI / 180 * Math.abs((60 * hours + minutes) - 12 * minutes);
 }
 
 
